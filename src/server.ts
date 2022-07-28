1f22561fa0b701aa4fb33503944ad20697123305
Adam Pohl <acpohl21@gmail.com>
2022-07-28 19:28:56 -0400

    Basic routing for server implemented

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,28 @@ export function startServer(){
 
 
     const requestListener = function (req: IncomingMessage, res: ServerResponse) {
-        res.writeHead(200);
-        res.end("Built a server");
+        res.setHeader("Content-Type", "text/plain");
+        // Set HTTP Code as 200 by default, only change if error occurs
+        switch(req.url) {
+            case "/command":
+                res.writeHead(200);
+                res.end("Hit command");
+                break
+            case "/new":
+                res.writeHead(200);
+                res.end("Hit new");
+                break
+            default:
+                res.writeHead(404);
+                res.end("Resource not found");
+        }
+        // res.end("Built a server");
     }
 
     const server: Server = createServer(requestListener);
     server.listen(port, host, () => {
         console.log(`Server is running on http://${host}:${port}`);
     });
+
+    server.listen
 }
