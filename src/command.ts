908b2e649fb76297108040df034994f2c453101e
Adam Pohl <acpohl21@gmail.com>
2022-07-28 16:17:03 -0400

    Continuing to extend commands

--- src/command.ts
+++ src/command.ts
@@ -49,18 +49,26 @@ export function createCommandParser(): Command{
         .version("0.0.1");
     
     // TODO: Ensure these debug commands have access restriction
+    program.command("debug")
+        .description("A suite of debug and admin tools for the clubstats server")
+        .argument("<subcommand>", "The subcommand to run")
+        .action((subcommand) => {
+            console.log(`running debug subcommand ${subcommand}`);
+        });
+
     program.command("load")
         .description("Load Matrix data from a provided JSON file")
         .argument("<file>", "File path to the JSON file to read from")
         .action((file) => {
             console.log("Loading from file %s", file);
         });
+
     program.command("connect")
         .description("Connect to a dgraph database")
         .argument("<endpoint>", "Endpoint URL for the dgraph server")
         .action((endpoint) => {
             console.log("Creating connection for database at %s", endpoint);
-        })
+        });
     
     return program
 }
