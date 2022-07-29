47218941bf992476892d221a44ccc03e84eccab9
Adam Pohl <acpohl21@gmail.com>
2022-07-29 02:43:23 -0400

    Query work

--- src/queries/query.ts
+++ src/queries/query.ts
@@ -1,19 +1,51 @@
 import { Txn } from "dgraph-js";
 
-const QUERY_START = 'query ';
+// Consant strings to build queries out of
+
+const QUERY_TEMPLATE = `{
+NAME(func: FUNC(PREDICATE, COMPARISON)){
+    RETURNS
+    }
+}`
+
+const QUERY_START = '{query(';
+
+const FUNC_START = 'func: '
+const FUNC_ALLOFTERMS = 'allofterms(';
+const FUNC_ANYOFTERMS = 'anyofterms(';
+const FUNC_EQ = 'eq(';
+
+enum Fnc {
+    ALLOFTERMS = 'allofterms(',
+    ANYOFTERMS = 'anyofterms(',
+    EQ = 'eq(',
+};
 
 export class Query {
     
     vars: JSON | undefined
-    body: string
     // Transaction object from dgraph client
     txn: Txn
+    private body: string
+    
+    private fnc: Fnc | undefined
+    private predicate: string | undefined
 
     constructor(txn: Txn, vars?: JSON){
         this.txn = txn;
-        this.body = QUERY_START
+        this.body = QUERY_START;
         this.vars = vars;
     }
 
+    addFunction(fnc: Fnc){
+        this.fnc = fnc;
+    }
+
+    buildQuery(){
+        this.body += QUERY_START;
+        if(typeof this.fnc != 'undefined'){
+            this.body += FUNC_START + this.fnc + this.predicate
+        }
+    }
 
 }
